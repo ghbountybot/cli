ba075b800e873bc1cf02e706371bc915daacf9ad
Andrew Gazelka <andrew.gazelka@gmail.com>
2024-12-28 20:35:18 -0800

    perf: smaller Docker image

--- flake.nix
+++ flake.nix
@@ -17,9 +17,20 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        inherit (nixpkgs) lib;
         
+        # Use static builds for Docker
+        pkgsStatic = import nixpkgs {
+          inherit system overlays;
+          crossSystem = {
+            config = "x86_64-unknown-linux-musl";
+            isStatic = true;
+          };
+        };
+
         rustToolchain = pkgs.rust-bin.stable.latest.default.override {
           extensions = [ "rust-src" "rust-analyzer" ];
+          targets = [ "x86_64-unknown-linux-musl" ];
         };
 
         nativeBuildInputs = with pkgs; [
@@ -32,8 +43,8 @@
           libgit2
         ];
 
-        # Define the package separately so we can reuse it in the Docker image
-        package = pkgs.rustPlatform.buildRustPackage {
+        # Native package for local development and running
+        nativePackage = pkgs.rustPlatform.buildRustPackage {
           pname = "bounty-cli";
           version = "0.1.0";
           src = ./.;
@@ -43,6 +54,45 @@
           };
 
           inherit buildInputs nativeBuildInputs;
+        };
+
+        # Static package for Docker
+        dockerPackage = pkgsStatic.rustPlatform.buildRustPackage {
+          pname = "bounty-cli";
+          version = "0.1.0";
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = with pkgsStatic; [
+            pkg-config
+            pkgsStatic.stdenv.cc
+          ];
+
+          buildInputs = with pkgsStatic; [
+            openssl.dev
+            openssl.out
+            libgit2.dev
+            zlib.dev
+          ];
+
+          # Enable static linking
+          CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+          OPENSSL_STATIC = "1";
+          OPENSSL_LIB_DIR = "${pkgsStatic.openssl.out}/lib";
+          OPENSSL_INCLUDE_DIR = "${pkgsStatic.openssl.dev}/include";
+          OPENSSL_NO_VENDOR = "1";
+          LIBGIT2_SYS_USE_PKG_CONFIG = "1";
+          PKG_CONFIG_ALL_STATIC = "1";
+          PKG_CONFIG_PATH = "${pkgsStatic.openssl.dev}/lib/pkgconfig:${pkgsStatic.libgit2.dev}/lib/pkgconfig";
+          
+          # Force static linking
+          NIX_LDFLAGS = "-L${pkgsStatic.openssl.out}/lib -lssl -lcrypto";
+          
+          # Use musl target
+          RUSTFLAGS = "-C target-feature=+crt-static";
         };
 
       in
@@ -56,14 +106,23 @@
 
         # Packages
         packages = {
-          default = package;
+          default = nativePackage;
           docker = pkgs.dockerTools.buildLayeredImage {
             name = "bounty-cli";
             tag = "latest";
             
+            # Use a minimal base
+            fromImage = pkgs.dockerTools.buildImage {
+              name = "scratch";
+              tag = "latest";
+            };
+
             contents = [
-              package
-              pkgs.cacert  # Only include SSL certificates
+              (pkgs.runCommand "minimal-runtime" {} ''
+                mkdir -p $out/etc/ssl/certs
+                cp ${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt $out/etc/ssl/certs/
+              '')
+              dockerPackage
             ];
 
             config = {
@@ -77,12 +136,12 @@
 
         # Apps
         apps.default = flake-utils.lib.mkApp {
-          drv = package;
+          drv = nativePackage;
         };
 
         # Optional: Overlay to make the package available in other flakes
         overlays.default = final: prev: {
-          bounty-cli = package;
+          bounty-cli = nativePackage;
         };
       }
     );
